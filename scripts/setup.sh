964a94e9af3874545aaad92e6d3e31ab662d05dd
Aren Sandersen <aren@foxpass.com>
2016-06-14 18:42:38 -0700

    Pass AWS AMI security check

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -22,6 +22,10 @@ EOF
 
 make programs && make install
 
+# delete libreswan source
+cd /opt/src
+rm -rf "libreswan-$SWAN_VER"
+
 echo > /var/tmp/libreswan-nss-pwd
 /usr/bin/certutil -N -f /var/tmp/libreswan-nss-pwd -d /etc/ipsec.d
 /bin/rm -f /var/tmp/libreswan-nss-pwd

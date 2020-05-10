f7206d81bc0fb9d437d1eea0e7164b9058b61079
Travis Theune <travist@tenminutesout.net>
2020-05-10 21:38:27 +0200

    Ubuntu 20.04 and python3.8 (#24)
    * Ubuntu 20.04, work in progress
    
    * Updates for python3.8 and ubuntu20.04
    
    * setup.sh and packer config updates
    
    * cleanup
    
    * Update default source_ami
    
    * typos
    
    * typos
    
    * Resovled bugs
    
    * Bring in Aren's ipsec.conf changes
    
    * Restore radiusclient.conf
    
    * Update Ubuntu line in build instructions
    
    * Move ownership of foxpass-radius-agent.conf to nobody:nogroup
    cleanup Dockerfile
    
    * Changes for packer install
    
    * Debugging final build
    
    * typo
    
    Co-authored-by: Aren Sandersen <aren@foxpass.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,9 +3,9 @@
 # quit if any command fails
 set -e
 
-mkdir /opt/src
+mkdir -p /opt/src
 cd /opt/src
-SWAN_VER=3.18
+SWAN_VER=3.31
 SWAN_FILE="libreswan-${SWAN_VER}.tar.gz"
 SWAN_URL="https://download.libreswan.org/$SWAN_FILE"
 wget -t 3 -T 30 -nv -O "$SWAN_FILE" "$SWAN_URL"
@@ -27,13 +27,8 @@ echo > /var/tmp/libreswan-nss-pwd
 /usr/bin/certutil -N -f /var/tmp/libreswan-nss-pwd -d /etc/ipsec.d
 /bin/rm -f /var/tmp/libreswan-nss-pwd
 
-# install libreswan into systemd
-systemctl enable ipsec.service
-systemctl start ipsec.service
-
 touch /etc/ipsec.conf
 touch /etc/iptables.rules
 touch /etc/ipsec.secrets
 touch /etc/ppp/options.xl2tpd
 touch /etc/xl2tpd/xl2tpd.conf
-touch /etc/radiusclient/port-id-map

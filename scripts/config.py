1766b07e9a451ded84af25c30c9b983de87669ac
Bryan <bojorquebryan@gmail.com>
2022-06-30 13:02:18 +0800

    fix xauth configurations

--- scripts/config.py
+++ scripts/config.py
@@ -179,10 +179,15 @@ def gather_user_data_s3(s3_url):
 
     config = json.loads(data)
 
-    # if it has 'local_cidr', then use that value for l2tp_cidr
-    local_cidr = config.pop('local_cidr', None)
-    if local_cidr:
-        config['l2tp_cidr'] = local_cidr
+    # if it has 'l2tp_cidr', then use that value for l2tp_cidr
+    l2tp_cidr = config.pop('l2tp_cidr', None)
+    if l2tp_cidr:
+        config['l2tp_cidr'] = l2tp_cidr
+
+    # if it has 'xauth_cidr', then use that value for xauth_cidr
+    xauth_cidr = config.pop('xauth_cidr', None)
+    if xauth_cidr:
+        config['xauth_cidr'] = xauth_cidr
 
     return config
 
@@ -190,10 +195,15 @@ def gather_user_data_s3(s3_url):
 def gather_user_data_file(filename):
     config = json.load(open(filename))
 
-    # if it has 'local_cidr', then use that value for l2tp_cidr
-    local_cidr = config.pop('local_cidr', None)
-    if local_cidr:
-        config['l2tp_cidr'] = local_cidr
+    # if it has 'l2tp_cidr', then use that value for l2tp_cidr
+    l2tp_cidr = config.pop('l2tp_cidr', None)
+    if l2tp_cidr:
+        config['l2tp_cidr'] = l2tp_cidr
+
+    # if it has 'xauth_cidr', then use that value for xauth_cidr
+    xauth_cidr = config.pop('xauth_cidr', None)
+    if xauth_cidr:
+        config['xauth_cidr'] = xauth_cidr
 
     return config
 
